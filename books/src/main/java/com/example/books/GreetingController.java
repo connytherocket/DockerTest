15fb4e768ce21fb3bfb4d91896aa9afaf5a5732a
eekmekciog <eekmekciog@stud.hs-heilbronn.de>
2023-11-14 15:04:42 +0100

    hello2

--- books/src/main/java/com/example/books/GreetingController.java
+++ books/src/main/java/com/example/books/GreetingController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return  String.format("Hello, %s!", name);
+        return  String.format("Helo, %s!", name);
     }
 }
